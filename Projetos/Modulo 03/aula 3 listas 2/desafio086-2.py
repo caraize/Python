ec4ff2b24fa49b66790ec8a34afd71bd238a97a7
Gabriel Nascimento <gabriel_nascimento12@outlook.com>
2025-07-14 22:46:39 -0300

    Ajeitando exercícios
    Colocando enunciado e a ajeitando exercicios

--- Projetos/Modulo 03/aula 3 listas 2/desafio086-2.py
+++ Projetos/Modulo 03/aula 3 listas 2/desafio086-2.py
@@ -1,3 +1,5 @@
+''' Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
+'''
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 for l in range(3):
     for c in range(3):
--- Projetos/Modulo 03/aula 3 listas 2/desafio087.py
+++ Projetos/Modulo 03/aula 3 listas 2/desafio087.py
@@ -1,3 +1,7 @@
+'''Aprimore o desafio anterior, mostrando no final: 
+A) A soma de todos os valores pares digitados.
+B) A soma dos valores da terceira coluna.
+C) O maior valor da segunda linha.'''
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 somapar = mai = somacoluna = 0
 
